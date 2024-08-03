2facf719f2002d8e961eb10071734400a46314bd
Enrico Rampazzo <iarendis@gmail.com>
2024-08-03 07:50:47 +0000

    now using grouped graph chart

--- helpers/data_helpers.py
+++ helpers/data_helpers.py
@@ -12,5 +12,18 @@ def get_stats_per_prompt():
     """
 
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent.parent/'data/stats_per_prompt.json'
+    DATA_FILENAME = Path(__file__).parent.parent/'data/stats_per_prompt-merged.json'
+    return pd.read_json(DATA_FILENAME, orient="index")
+
+@st.cache_data
+def get_stats_per_prompt_battery():
+    """Grab benchmark data from a JSON file.
+
+    This uses caching to avoid having to read the file every time. If we were
+    reading from an HTTP endpoint instead of a file, it's a good idea to set
+    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
+    """
+
+    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
+    DATA_FILENAME = Path(__file__).parent.parent/'data/stats_per_prompt-battery.json'
     return pd.read_json(DATA_FILENAME)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,13 @@
 import streamlit as st
 import pandas as pd
-from helpers.data_helpers import get_stats_per_prompt
-
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
-
-
+from helpers.data_helpers import get_stats_per_prompt, get_stats_per_prompt_battery
+import altair as alt
 
 stats_per_prompt = get_stats_per_prompt()
+stats_per_prompt_battery = get_stats_per_prompt_battery()
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
 '''
-### :bar_chart: Tokens per second for each prompt
+### :bar_chart: Tokens per second for each prompt :electric_plug: vs :battery:
 '''
 
-st.bar_chart(stats_per_prompt.transpose().loc[:,"avg_tokens_per_second"], use_container_width=True)
-
-
+st.bar_chart(stats_per_prompt.loc[:,["avg_tokens_per_second", "avg_tokens_per_second_battery"]])
