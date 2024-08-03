3d195f676d588f5d5fdcc53c2bea4df103a86058
Enrico Rampazzo <iarendis@gmail.com>
2024-08-03 16:13:11 +0000

    removed unused functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
-from helpers.data_helpers import get_stats_per_prompt, get_stats_per_prompt_battery
+from helpers.data_helpers import get_stats_per_prompt
 import altair as alt
 
 stats_per_prompt = get_stats_per_prompt()
-stats_per_prompt_battery = get_stats_per_prompt_battery()
 
 '''
 ### :bar_chart: Tokens per second for each prompt :electric_plug: vs :battery:
